24e3a3a8958eef48694183d2a25e072b0b0f5b40
ThePrince-Verma <prince.pv759@gmail.com>
2024-11-30 18:07:51 +0530

    made change in content of projects

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -13,7 +13,7 @@ const Works = () => {
     <section id='works'>
       <h2 className="worksTitle">My Projects</h2>
       <span className="worksDesc">
-        I take pride in paying attention to the smallest details and
+        I take pride in paying attention to the smallest details and making sure that my work is pixel perfect.I am excited to bring my skills and experience to help companies/businesses achieve their goals and create a strong online presence.
       </span>
       <div className="worksImgs">
         <a href="https://quiz-on-react.netlify.app/">
